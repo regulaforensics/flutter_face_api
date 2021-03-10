a4ef1d8071c9a0db57d7141f3e4bb6c023d065a4
Jenkins <jenkins@regula.by>
2021-03-10 15:00:47 +0300

    0.0.7

--- example/android/app/src/main/java/io/flutter/plugins/regula/faceapi/flutter_face_api_example/MainActivity.java
+++ example/android/app/src/main/java/io/flutter/plugins/regula/faceapi/flutter_face_api_example/MainActivity.java
@@ -1,4 +1,4 @@
-package io.flutter.plugins.regula.documentreader.flutter_document_reader_api_example;
+package io.flutter.plugins.regula.faceapi.flutter_face_api_example;
 
 import io.flutter.embedding.android.FlutterActivity;
 
