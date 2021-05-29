6880a6a52f9a7c20b5c9040090465d407b3336ec
Jenkins <jenkins@regula.by>
2021-05-29 15:43:36 +0300

    0.0.18

--- ios/Classes/FlutterFaceApiPlugin.m
+++ ios/Classes/FlutterFaceApiPlugin.m
@@ -37,8 +37,8 @@ typedef void (^Callback)(NSString* response);
         [self stopFaceCaptureActivity :successCallback :errorCallback];
     else if([action isEqualToString:@"stopLivenessProcessing"])
         [self stopLivenessProcessing :successCallback :errorCallback];
-    // else if([action isEqualToString:@"presentFaceCaptureActivityByCameraId"])
-    //     [self presentFaceCaptureActivityByCameraId :[args objectAtIndex:0] :successCallback :errorCallback];
+    else if([action isEqualToString:@"presentFaceCaptureActivityByCameraId"])
+        [self presentFaceCaptureActivityByCameraId :[args objectAtIndex:0] :successCallback :errorCallback];
     else if([action isEqualToString:@"startLivenessByCameraId"])
         [self startLivenessByCameraId :[args objectAtIndex:0] :successCallback :errorCallback];
     else if([action isEqualToString:@"setServiceUrl"])
@@ -83,7 +83,7 @@ typedef void (^Callback)(NSString* response);
     [self result:@"this is an android-only method" :errorCallback];
 }
 
-- (void) startLivenessMatchingByCameraId:(NSNumber*)cameraId : (Callback)successCallback :(Callback)errorCallback{
+- (void) startLivenessByCameraId:(NSNumber*)cameraId : (Callback)successCallback :(Callback)errorCallback{
     [self result:@"this is an android-only method" :errorCallback];
 }
 
