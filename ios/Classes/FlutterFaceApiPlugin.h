ffc1c2b6c5c93f43c6d69f977b8472d35bc85ef2
pavelmasuk <Siarhei.Homan@regula.by>
2024-11-28 15:55:41 +0300

    6.3.749-beta

--- ios/Classes/FlutterFaceApiPlugin.h
+++ ios/Classes/FlutterFaceApiPlugin.h
@@ -1,10 +1,28 @@
 #import <Flutter/Flutter.h>
-#import "RFSWMain.h"
+#import <FaceSDK/FaceSDK.h>
+#import "RFSWJSONConstructor.h"
+#import "RFSWConfig.h"
 
-@interface FlutterFaceApiPlugin : NSObject<FlutterPlugin>
+typedef void (^RFSWCallback)(id _Nullable response);
+typedef void (^RFSWEventSender)(NSString* _Nonnull event, id _Nullable data);
+
+@interface FlutterFaceApiPlugin : NSObject<FlutterPlugin,
+                                            RFSURLRequestInterceptingDelegate,
+                                            RFSVideoUploadingDelegate,
+                                            RFSCustomizationActionDelegate,
+                                            RFSFaceCaptureDelegate,
+                                            RFSLivenessDelegate>
+
+@property (strong, nonatomic, class) FlutterMethodChannel* _Nullable channel;
+@property NSDictionary* _Nullable headers;
+
 @end
 
-static NSMutableDictionary<NSString*, FlutterEventSink>* eventSinks;
+
+NSString* _Nonnull RFSWCameraSwitchEvent;
+NSString* _Nonnull RFSWLivenessNotificationEvent;
+NSString* _Nonnull RFSWVideoEncoderCompletionEvent;
+NSString* _Nonnull RFSWOnCustomButtonTappedEvent;
 
 @interface RFSWCameraSwitchStreamHandler : NSObject <FlutterStreamHandler>
 @end
