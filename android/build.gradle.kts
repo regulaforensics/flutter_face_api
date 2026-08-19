4ca665e7b2e83ec37dc322fa7d2e7ddb38c2d0cc
Jenkins <jenkins@regula.by>
2026-08-19 06:19:27 +0300

    8.4.1316-rc

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -56,7 +56,7 @@ extensions.configure<KotlinAndroidProjectExtension> {
 }
 
 dependencies {
-    implementation("com.regula.face:api:8.4.5243") {
+    implementation("com.regula.face:api:8.4.5248") {
         isTransitive = true
     }
 
