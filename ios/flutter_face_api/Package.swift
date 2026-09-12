f4c7e1a2e37483899691ccccbfcaf18cfa4071cc
Jenkins <jenkins@regula.by>
2026-09-12 06:19:00 +0300

    8.4.1494-rc

--- ios/flutter_face_api/Package.swift
+++ ios/flutter_face_api/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .package(name: "FlutterFramework", path: "../FlutterFramework"),
         .package(
             url: "https://github.com/regulaforensics/FaceSDK-Swift-Package",
-            exact: "8.4.4888-rc"
+            exact: "8.4.4892-rc"
         ),
     ],
     targets: [
