a916f036d8e8f1085fb0925a9c9c37ff1ec6a744
Jenkins <jenkins@regula.by>
2026-09-15 01:43:41 +0300

    8.3.1505-nightly

--- ios/flutter_face_api/Sources/flutter_face_api/FlutterFaceApiPlugin.swift
+++ ios/flutter_face_api/Sources/flutter_face_api/FlutterFaceApiPlugin.swift
@@ -42,17 +42,25 @@ class GenericStreamHandler: NSObject, FlutterStreamHandler {
 
 var eventSinks: [String: FlutterEventSink] = [:]
 func sendEvent(_ event: String, _ data: Any? = nil) {
-    DispatchQueue.main.async {
-        if let sink = eventSinks[event] {
-            sink(data.toSendable())
-        }
-    }
+    runAsync { _ in eventSinks[event]?(data.toSendable()) }
 }
 
-let rootViewController: () -> UIViewController? = {
-    return UIApplication.shared.connectedScenes
-        .compactMap { $0 as? UIWindowScene }
-        .flatMap { $0.windows }
-        .first { $0.isKeyWindow }?
-        .rootViewController
+func runAsync(_ action: @escaping (UIViewController) -> Void) {
+    DispatchQueue.main.async {
+        let windows = UIApplication.shared.connectedScenes.compactMap { $0 as? UIWindowScene }.filter { $0.activationState == .foregroundActive || $0.activationState == .foregroundInactive }.flatMap { $0.windows }
+        let candidates = windows.filter { $0.isKeyWindow } + windows.filter { !$0.isKeyWindow && !$0.isHidden && $0.alpha > 0 && $0.windowLevel == .normal }
+        for window in candidates {
+            guard var presenter = window.rootViewController else { continue }
+            while let next = presenter.presentedViewController
+                ?? (presenter as? UINavigationController)?.visibleViewController
+                ?? (presenter as? UINavigationController)?.topViewController
+                ?? (presenter as? UITabBarController)?.selectedViewController {
+                presenter = next
+            }
+            guard presenter.viewIfLoaded?.window != nil else { continue }
+            action(presenter)
+            return
+        }
+        print("REGULA: Cannot present the UI: no presenter available.")
+    }
 }
